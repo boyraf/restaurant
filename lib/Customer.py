f202ecf5b96d97e44754b0b1fae3ce3a0bcbfec5
boyraf <raphael.kymotho@gmail.com>
2023-06-05 00:29:10 +0300

    commit

--- lib/Customer.py
+++ lib/Customer.py
@@ -1,2 +1,43 @@
 class Customer:
-    pass
+
+    customers = []
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.customers.append(self)
+        self.reviews = []
+
+
+    def given_name(self):
+        return self.first_name
+    
+    def family_name(self):
+        return self.last_name
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    @classmethod
+    def all(cls):
+        return cls.customers
+    
+    def num_reviews(self):
+        return len(self.reviews)
+    
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.customers:
+            if customer.full_name() == name:
+                return customer
+            else:
+                return None
+            
+    @classmethod
+    def find_all_by_given_name(cls, name):
+        found_name = []
+        for customer in cls.customers:
+            if customer.given_name() == name:
+                found_name.append(customer)
+                return found_name
+     
--- lib/Review.py
+++ lib/Review.py
@@ -1,2 +1,15 @@
 class Review:
-    pass
+    def __init__(self, customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = rating
+
+    def rating(self):
+        return self._rating
+
+    def customer(self):
+        return self._customer
+
+    def restaurant(self):
+        return self._restaurant
+
